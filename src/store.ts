b4e7a3be59c5b42efa0b5501f205e46bc2a333f4
Nguyen Hong Son <snguyenhong8@gmail.com>
2025-10-30 00:38:17 +0700

    pause

---
+++ src/store.ts
@@ -0,0 +1,15 @@
+import { configureStore} from "@reduxjs/toolkit";
+
+
+// Main function of RTK in order to create Redux store
+// It's more convenient than createStore because it make a lot of for you automatically
+// conbine reducer, add middleware for thunk, set up redux devtools
+export const store = configureStore({
+    reducer: {},
+})
+
+// Infer the RootState and AppDispatch types form the store itself
+export type RootState = ReturnType<typeof store.getState>
+
+// Inferred type: {posts: PostState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
